25512df78daf2ba6849454791ccb49d74301da63
Jaysoni14-3 <jaysoni998.js@gmail.com>
2023-05-03 03:30:12 +0530

    Fixed responsiveness

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 var navigationBar = document.querySelector(".navigation-bar");
 var hamburgerIcon = document.querySelector(".hamburger-menu-icon");
+var hamburgerIconClose = document.querySelector(".hamburger-menu-icon-close");
 var body = document.querySelector("body");
 
+
+hamburgerIconClose.addEventListener("click", function(){
+    if(navigationBar.classList.contains("show")){
+        navigationBar.classList.remove("show");
+        body.classList.remove("noScroll-body");
+    }
+});    
 
 hamburgerIcon.addEventListener("click", function(){
     if(navigationBar.classList.contains("show")){
